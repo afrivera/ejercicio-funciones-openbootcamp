b998d4fe42f11865de9e5839b3c2e99ec96912f9
afrivera <andresrfelip@hotmail.com>
2022-12-26 15:30:48 -0500

    solución segunda parte

---
+++ src/Coche.java
@@ -0,0 +1,8 @@
+public class Coche {
+
+    public int puertas;
+
+    public void aumentarPuertas(){
+        puertas+= 1;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,14 @@ public class Main {
     
         int resultado = sumarTresParametros(10, 20, 30);
         System.out.println("resultado = " + resultado);
-        
+
+        Coche coche = new Coche();
+        coche.aumentarPuertas();
+        coche.aumentarPuertas();
+        coche.aumentarPuertas();
+        coche.aumentarPuertas();
+        System.out.println("cantidad de puertas = " + coche.puertas);
+
     }
     
     public static int sumarTresParametros(int a, int b, int c){
