62073b7bc07b1be3953539b3891be2df9567f81b
afrivera <andresrfelip@hotmail.com>
2022-12-26 15:26:42 -0500

    solucion primera parte

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    
+        int resultado = sumarTresParametros(10, 20, 30);
+        System.out.println("resultado = " + resultado);
+        
+    }
+    
+    public static int sumarTresParametros(int a, int b, int c){
+        return a + b + c;
     }
 }
