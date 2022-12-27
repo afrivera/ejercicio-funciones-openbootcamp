d7785b4f302d4911dcbfb6871fad93ba434546cb
afrivera <andresrfelip@hotmail.com>
2022-12-27 15:22:47 -0500

    se instancia clase Persona en Clase Main y se setean e imprimen valores

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        Persona persona1 = new Persona();
+        persona1.setEdad(15);
+        persona1.setNombre("Andres");
+        persona1.setTelefono("323323323");
+
+        Persona persona2 = new Persona();
+        persona2.setEdad(20);
+        persona2.setNombre("Paola");
+        persona2.setTelefono("323232323");
+
+        // imprimir persona 1
+        System.out.println(persona1.getEdad());
+        System.out.println(persona1.getNombre());
+        System.out.println(persona1.getTelefono());
+
+        System.out.println("-".repeat(40));
+        // imprimer Persona 2
+        System.out.println(persona2.getEdad());
+        System.out.println(persona2.getNombre());
+        System.out.println(persona2.getTelefono());
+
     }
 }
