2412970c62ce9efd9993c04375b1e560941a6881
afrivera <andresrfelip@hotmail.com>
2022-12-27 17:10:59 -0500

    se crea clase Cliente y se hereda de Clase Persona, se setean e imprimen los valores de las propiedades del cliente

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Persona persona1 = new Persona();
-        persona1.setEdad(15);
-        persona1.setNombre("Andres");
-        persona1.setTelefono("323323323");
+        Cliente cliente1 = new Cliente();
+        cliente1.setEdad(15);
+        cliente1.setNombre("Andres");
+        cliente1.setTelefono("323323323");
+        cliente1.setCredito(12345678);
 
-        Persona persona2 = new Persona();
-        persona2.setEdad(20);
-        persona2.setNombre("Paola");
-        persona2.setTelefono("323232323");
+        System.out.println("Cliente 1:");
+        System.out.println("Edad: " + cliente1.getEdad());
+        System.out.println("Nombre: " + cliente1.getNombre());
+        System.out.println("Telefono: " + cliente1.getTelefono());
+        System.out.println("Credito: " + cliente1.getCredito());
 
-        // imprimir persona 1
-        System.out.println(persona1.getEdad());
-        System.out.println(persona1.getNombre());
-        System.out.println(persona1.getTelefono());
 
-        System.out.println("-".repeat(40));
-        // imprimer Persona 2
-        System.out.println(persona2.getEdad());
-        System.out.println(persona2.getNombre());
-        System.out.println(persona2.getTelefono());
 
     }
 }
