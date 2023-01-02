b3e4cccfa7a84a79a0816f8579d5dccabdcda4ab
afrivera <andresrfelip@hotmail.com>
2023-01-02 14:08:33 -0500

    tarea openbootcamp solucion ejercicio 1

--- src/Coche.java
+++
@@ -1,8 +0,0 @@
-public class Coche {
-
-    public int puertas;
-
-    public void aumentarPuertas(){
-        puertas+= 1;
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-    
-        int resultado = sumarTresParametros(10, 20, 30);
-        System.out.println("resultado = " + resultado);
 
-        Coche coche = new Coche();
-        coche.aumentarPuertas();
-        coche.aumentarPuertas();
-        coche.aumentarPuertas();
-        coche.aumentarPuertas();
-        System.out.println("cantidad de puertas = " + coche.puertas);
+        // enteros
+        byte variableByte = 4;
+        short variableShort = 9;
+        int variableInt = 1234;
+        long variableLong = 12346689;
+
+        System.out.println(variableByte);
+        System.out.println(variableShort);
+        System.out.println(variableInt);
+        System.out.println(variableLong);
+
+        System.out.println();
+        // decimales
+        float variableFloat = 12.4f;
+        double variableDouble = 566.9d;
+        System.out.println(variableFloat);
+        System.out.println(variableDouble);
+
+        System.out.println();
+
+        // booleans
+        boolean variableBoolean = true;
+        boolean variableBoolean1 = false;
+        System.out.println(variableBoolean);
+        System.out.println(variableBoolean1);
+
+        System.out.println();
+
+        // texto
+        char variableChar = 'A';
+        String variableString = "Andres Felipe Rivera";
+        System.out.println(variableChar);
+        System.out.println(variableString);
+
 
     }
-    
-    public static int sumarTresParametros(int a, int b, int c){
-        return a + b + c;
-    }
+
 }
